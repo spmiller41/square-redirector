32ddff006dad5f4efc15b661a1feb3c9d269caf1
Sean <spmiller41@gmail.com>
2025-04-10 16:22:35 -0400

    Commit update - testing successful invoice fetch from invoiceid

--- src/main/java/com/powersolutions/squarebridge/rest/ZohoRestController.java
+++ src/main/java/com/powersolutions/squarebridge/rest/ZohoRestController.java
@@ -1,18 +1,27 @@
 package com.powersolutions.squarebridge.rest;
 
+import com.powersolutions.squarebridge.zoho.ZohoInvoiceIntegrationService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/v1/api")
 public class ZohoRestController {
 
+    @Autowired
+    private ZohoInvoiceIntegrationService integrationService;
+
     @GetMapping("/create_payment")
-    public void createPayment(@RequestParam("invoice_id") String referenceNumber) {
-        System.out.println("Invoice Id: " + referenceNumber);
+    public void createPayment(@RequestParam("invoice_id") String invoiceId) {
+        System.out.println("Invoice Id: " + invoiceId);
+        Optional<String> optResponse = integrationService.getZohoInvoiceById(invoiceId);
+        optResponse.ifPresent(System.out::println);
     }
 
 }
