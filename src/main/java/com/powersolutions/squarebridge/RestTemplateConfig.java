d3d3fafb1947da6c70f2d5335b02abcc7ba2288e
Sean <spmiller41@gmail.com>
2025-04-10 14:34:01 -0400

    Commit rest temp config

--- src/main/java/com/powersolutions/squarebridge/RestTemplateConfig.java
+++ src/main/java/com/powersolutions/squarebridge/RestTemplateConfig.java
@@ -1,4 +1,15 @@
 package com.powersolutions.squarebridge;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
