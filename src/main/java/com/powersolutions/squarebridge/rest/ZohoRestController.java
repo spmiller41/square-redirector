2a273f165fb95a3c78feef714d43b07e00f0124b
Sean <spmiller41@gmail.com>
2025-04-14 16:40:14 -0400

    Commit update to handle and redirect paymnent requests

--- src/main/java/com/powersolutions/squarebridge/rest/ZohoRestController.java
+++ src/main/java/com/powersolutions/squarebridge/rest/ZohoRestController.java
@@ -1,8 +1,13 @@
 package com.powersolutions.squarebridge.rest;
 
+import com.powersolutions.squarebridge.service.CustomerCheckoutService;
+import com.powersolutions.squarebridge.square.SquareCheckoutIntegration;
 import com.powersolutions.squarebridge.zoho.ZohoInvoiceIntegrationService;
-import jakarta.servlet.http.HttpServletRequest;
+import com.powersolutions.squarebridge.zoho.dto.ZohoInvoiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,13 +20,19 @@ import java.util.Optional;
 public class ZohoRestController {
 
     @Autowired
-    private ZohoInvoiceIntegrationService integrationService;
+    private CustomerCheckoutService checkoutService;
 
     @GetMapping("/create_payment")
-    public void createPayment(@RequestParam("invoice_id") String invoiceId) {
-        System.out.println("Invoice Id: " + invoiceId);
-        Optional<String> optResponse = integrationService.getZohoInvoiceById(invoiceId);
-        optResponse.ifPresent(System.out::println);
+    public ResponseEntity<Void> createPayment(@RequestParam("invoice_id") String invoiceId) {
+        String checkoutLink = checkoutService.handleInvoiceCheckout(invoiceId);
+
+        if (checkoutLink == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, checkoutLink)
+                .build();
     }
 
 }
