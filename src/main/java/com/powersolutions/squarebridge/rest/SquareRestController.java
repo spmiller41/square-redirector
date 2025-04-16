a3ef6d196863d8c937e508139e73e6d44a8b12ef
spmiller41 <spmiller41@gmail.com>
2025-04-16 16:44:41 -0400

    Commit feature to create payment in zoho invoice

--- src/main/java/com/powersolutions/squarebridge/rest/SquareRestController.java
+++ src/main/java/com/powersolutions/squarebridge/rest/SquareRestController.java
@@ -1,9 +1,13 @@
 package com.powersolutions.squarebridge.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.powersolutions.squarebridge.entities.CustomerCheckout;
+import com.powersolutions.squarebridge.entities.Payment;
 import com.powersolutions.squarebridge.security.SquareSignatureVerifier;
+import com.powersolutions.squarebridge.service.CustomerCheckoutService;
 import com.powersolutions.squarebridge.service.PaymentUpdateService;
 import com.powersolutions.squarebridge.square.dto.SquarePaymentUpdate;
+import com.powersolutions.squarebridge.zoho.ZohoInvoiceIntegrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +22,19 @@ public class SquareRestController {
 
     private final SquareSignatureVerifier signatureVerifier;
     private final PaymentUpdateService paymentUpdateService;
+    private final ZohoInvoiceIntegrationService zohoIntegrationService;
+    private final CustomerCheckoutService customerCheckoutService;
+
 
     @Autowired
-    public SquareRestController(SquareSignatureVerifier signatureVerifier, PaymentUpdateService paymentUpdateService) {
+    public SquareRestController(SquareSignatureVerifier signatureVerifier,
+                                PaymentUpdateService paymentUpdateService,
+                                ZohoInvoiceIntegrationService zohoIntegrationService,
+                                CustomerCheckoutService customerCheckoutService) {
         this.signatureVerifier = signatureVerifier;
         this.paymentUpdateService = paymentUpdateService;
+        this.zohoIntegrationService = zohoIntegrationService;
+        this.customerCheckoutService = customerCheckoutService;
     }
 
     @PostMapping("/payment_updates")
@@ -37,7 +49,15 @@ public class SquareRestController {
         try {
             SquarePaymentUpdate paymentUpdate = new SquarePaymentUpdate(paymentUpdatePayload);
             logger.info("Received Square payment update: {}", paymentUpdate);
-            paymentUpdateService.handlePaymentUpdate(paymentUpdate);
+
+            Payment payment = paymentUpdateService.handlePaymentUpdate(paymentUpdate);
+            if (payment != null) {
+                CustomerCheckout customerCheckout = customerCheckoutService.findByPaymentRecord(payment);
+                if (customerCheckout != null) {
+                    zohoIntegrationService.createZohoPayment(customerCheckout, payment);
+                }
+            }
+
             return ResponseEntity.ok().build();
         } catch (JsonProcessingException ex) {
             logger.error("Error while attempting to parse Square payment update: {}", ex.getMessage());
